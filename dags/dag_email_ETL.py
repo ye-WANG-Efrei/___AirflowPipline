7e2989244321d4531ed7829c7e4a2449e57822e5
ye-WANG-Efrei <90457316+ye-WANG-Efrei@users.noreply.github.com>
2025-12-02 14:48:31 +0100

    update DAGs

--- dags/dag_email_ETL.py
+++ dags/dag_email_ETL.py
@@ -1,19 +1,20 @@
+#dag_email_ETL.py
 from airflow import DAG
 from airflow.operators.python import PythonOperator
 from datetime import datetime
+from Email.extract_from_s3 import extract_msg_from_s3
 
 
-def extract_from_s3(**context):
-    # … 下载邮件的逻辑 …
 
-    with DAG(
-        dag_id="extract_from_s3_dag",
-        start_date=datetime(2024, 1, 1),
-        schedule_interval=None,
-        catchup=False,
-    ) as dag:
-        extract_task = PythonOperator(
-            task_id="extract",
-            python_callable=extract_from_s3,
-        )
 
+with DAG(
+    dag_id="extract_from_s3_dag",
+    start_date=datetime(2024, 1, 1),
+    schedule_interval=None,
+    catchup=False,
+) as dag:
+
+    extract_task = PythonOperator(
+        task_id="extract",
+        python_callable=extract_from_s3,
+    )
